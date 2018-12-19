6d65e1cf0c2dafde188a13eadcc6332a88b5b706
avzhatkin <avzhatkin@gmail.com>
2018-12-19 21:32:24 +0300

    Updated to use constants from rules object

--- MyStrategy.cpp
+++ MyStrategy.cpp
@@ -3,35 +3,6 @@
 #include <cmath>
 using namespace std;
 
-#define ROBOT_MIN_RADIUS 1.0
-#define ROBOT_MAX_RADIUS 1.05
-#define ROBOT_MAX_JUMP_SPEED 15.0
-#define ROBOT_ACCELERATION 100.0
-#define ROBOT_NITRO_ACCELERATION 30.0
-#define ROBOT_MAX_GROUND_SPEED 30.0
-#define ROBOT_ARENA_E 0.0
-#define ROBOT_RADIUS 1.0
-#define ROBOT_MASS 2.0
-#define TICKS_PER_SECOND 60.0
-#define MICROTICKS_PER_TICK 100.0
-#define RESET_TICKS (2 * TICKS_PER_SECOND)
-#define BALL_ARENA_E 0.7
-#define BALL_RADIUS 2.0
-#define BALL_MASS 1.0
-#define MIN_HIT_E 0.4
-#define MAX_HIT_E 0.5
-#define MAX_ENTITY_SPEED 100.0
-#define MAX_NITRO_AMOUNT 100.0
-#define START_NITRO_AMOUNT 50.0
-#define NITRO_POINT_VELOCITY_CHANGE 0.6
-#define NITRO_PACK_X 20.0
-#define NITRO_PACK_Y 1.0
-#define NITRO_PACK_Z 30.0
-#define NITRO_PACK_RADIUS 0.5
-#define NITRO_PACK_AMOUNT 100.0
-#define NITRO_RESPAWN_TICKS (10 * TICKS_PER_SECOND)
-#define GRAVITY 30.0
-
 struct vec3 {
     double x = 0., y = 0., z = 0.;
 
@@ -130,13 +101,13 @@ void MyStrategy::act(const Robot& me, const Rules& rules, const Game& game, Acti
         vec3 ball_dir = (ball_pos - goal_pos);
         ball_dir.y = 0.;
         ball_dir.normalize();
-        ball_dir *= BALL_RADIUS;
+        ball_dir *= rules.BALL_RADIUS;
         target = ball_pos + ball_dir;
     }
 
     vec3 target_dir = target - me_pos;
     target_dir.y = 0.f;
-    double target_speed_d = TICKS_PER_SECOND * min(MAX_ENTITY_SPEED, target_dir.length());
+    double target_speed_d = rules.TICKS_PER_SECOND * min(rules.MAX_ENTITY_SPEED, target_dir.length());
     vec3 target_speed = target_dir.normal() * target_speed_d;
     target_speed.y = 0.;
 
@@ -146,7 +117,7 @@ void MyStrategy::act(const Robot& me, const Rules& rules, const Game& game, Acti
 
     vec3 ball_dir = ball_pos - me_pos;
     ball_dir.y = 0.;
-    if (ball_dir.length() < BALL_RADIUS * 1.5 && ball_dir.z > 0.) {
-        action.jump_speed = ROBOT_MAX_JUMP_SPEED;
+    if (ball_dir.length() < rules.BALL_RADIUS * 1.5 && ball_dir.z > 0.) {
+        action.jump_speed = rules.ROBOT_MAX_JUMP_SPEED;
     }
 }
