e904204dde9bdb2aa1c7278ca4a00f3caa7ba17c
Andrey Zhatkin <avzhatkin@gmail.com>
2019-01-11 15:19:04 +0300

    Рабочий симулятор

--- MyStrategy.cpp
+++ MyStrategy.cpp
@@ -86,7 +86,7 @@ TouchInfo CheckArenaCollision(const Entity& e)
         }
 
         real_t size_y = s_arena.height - (s_arena.top_radius + s_arena.bottom_radius) / 2.0_r;
-        real_t pos_y = e.pos.y - size_y - s_arena.bottom_radius;
+        real_t pos_y = e.pos.y - (size_y + s_arena.bottom_radius);
         if (abs(pos_y) <= size_y)
         {
             ret.depth = abs(e.pos.x) + e.radius - s_arena.width;
@@ -144,7 +144,7 @@ TouchInfo CheckArenaCollision(const Entity& e)
     if (abs(e.pos.x) >= (s_arena.width - s_arena.corner_radius))
     {
         real_t size_y = s_arena.height - (s_arena.top_radius + s_arena.bottom_radius) / 2.0_r;
-        real_t pos_y = e.pos.y - size_y - s_arena.bottom_radius;
+        real_t pos_y = e.pos.y - (size_y + s_arena.bottom_radius);
         ret.normal.x = (abs(e.pos.x) - (s_arena.width - s_arena.corner_radius)) * sign(e.pos.x);
         ret.normal.z = (abs(e.pos.z) - simple_box.z) * sign(e.pos.z);
         ret.normal.y = ret.normal.len();
@@ -221,7 +221,146 @@ TouchInfo CheckArenaCollision(const Entity& e)
         return ret;
     }
 
+    if (e.pos.y >= (s_arena.goal_height + s_arena.goal_side_radius))
+    {
+        if (e.pos.y <= (s_arena.height * 2.0_r - s_arena.top_radius))
+        {
+            ret.depth = abs(e.pos.z) + e.radius - s_arena.depth;
+            if (ret.depth > 0)
+            {
+                ret.normal = vec3(0.0_r, 0.0_r, sign(e.pos.z));
+                return ret;
+            }
 
+            ret.depth = 0.0_r;
+            return ret;
+        }
+
+        if (abs(e.pos.z) <= (s_arena.width - s_arena.top_radius))
+        {
+            ret.depth = e.pos.y + e.radius - s_arena.height * 2.0_r;
+            if (ret.depth > 0)
+            {
+                ret.normal = vec3(0.0_r, 1.0_r, 0.0_r);
+                return ret;
+            }
+
+            ret.depth = 0.0_r;
+            return ret;
+        }
+
+        ret.normal.z = (abs(e.pos.z) - (s_arena.depth - s_arena.top_radius)) * sign(e.pos.z);
+        ret.normal.y = e.pos.y - (s_arena.height * 2.0_r - s_arena.top_radius);
+        ret.depth = ret.normal.len() + e.radius - s_arena.top_radius;
+        if (ret.depth > 0)
+        {
+            ret.normal.normalize();
+            return ret;
+        }
+
+        ret.depth = 0.0_r;
+        return ret;
+    }
+
+    if (abs(e.pos.z) < (s_arena.depth - s_arena.bottom_radius))
+    {
+        ret.depth = e.radius - e.pos.y;
+        if (ret.depth > 0)
+        {
+            ret.normal = vec3(0.0_r, -1.0_r, 0.0_r);
+            return ret;
+        }
+
+        ret.depth = 0.0_r;
+        return ret;
+    }
+
+    if (abs(e.pos.x) <= (s_arena.goal_width - s_arena.goal_top_radius) / 2.0_r)
+    {
+        ret.depth = e.radius - e.pos.y;
+        if (ret.depth > 0)
+        {
+            ret.normal = vec3(0.0_r, -1.0_r, 0.0_r);
+            return ret;
+        }
+
+        ret.normal.y = (s_arena.goal_height + s_arena.goal_side_radius) - e.pos.y;
+        ret.normal.z = ((s_arena.depth + s_arena.goal_side_radius) - abs(e.pos.z)) * sign(e.pos.z);
+        ret.depth = (e.radius + s_arena.goal_side_radius) - ret.normal.len();
+        if (ret.depth > 0)
+        {
+            ret.normal.normalize();
+            return ret;
+        }
+
+        ret.depth = 0.0_r;
+        return ret;
+    }
+
+    if (e.pos.x <= (s_arena.depth + s_arena.goal_side_radius))
+    {
+        ret.normal.x = ((s_arena.goal_width + s_arena.goal_side_radius) / 2.0_r - abs(e.pos.x)) * sign(e.pos.x);
+        ret.normal.z = ((s_arena.depth + s_arena.goal_side_radius) - abs(e.pos.z)) * sign(e.pos.z);
+        ret.depth = ret.normal.len();
+        if (e.pos.y < s_arena.bottom_radius)
+        {
+            if (ret.depth > s_arena.bottom_radius)
+            {
+                ret.depth = e.radius - e.pos.y;
+                if (ret.depth > 0)
+                {
+                    ret.normal = vec3(0.0_r, 1.0_r, 0.0_r);
+                    return ret;
+                }
+
+                ret.depth = 0.0_r;
+                return ret;
+            }
+
+            ret.normal.y = s_arena.bottom_radius;
+            ret.normal = e.pos - ret.normal;
+            ret.depth = ret.normal.len() + e.radius - s_arena.bottom_radius;
+            if (ret.depth > 0)
+            {
+                ret.normal.normalize();
+                return ret;
+            }
+
+            ret.depth = 0.0_r;
+            return ret;
+        }
+
+        if (e.pos.y <= (s_arena.goal_height - s_arena.goal_top_radius))
+        {
+            ret.depth = e.radius + s_arena.goal_side_radius - ret.depth;
+            if (ret.depth > 0)
+            {
+                ret.normal.normalize();
+                return ret;
+            }
+
+            ret.depth = 0.0_r;
+            return ret;
+        }
+
+        ret.normal.x = 0.0_r;
+        ret.normal.y = e.pos.y - (s_arena.goal_height - s_arena.goal_top_radius);
+        ret.normal.z = e.pos.z - (s_arena.depth + s_arena.goal_side_radius);
+        ret.normal = ret.normal.normal() * (s_arena.goal_top_radius + s_arena.goal_side_radius);
+        ret.normal.x = s_arena.depth + s_arena.goal_side_radius;
+        ret.normal.y += s_arena.goal_height - s_arena.goal_top_radius;
+        ret.normal.z = s_arena.depth + s_arena.goal_side_radius;
+        ret.normal = ret.normal - e.pos;
+        ret.depth = e.radius + s_arena.goal_side_radius - ret.normal.len();
+        if (ret.depth > 0)
+        {
+            ret.normal.normalize();
+            return ret;
+        }
+
+        ret.depth = 0.0_r;
+        return ret;
+    }
 
     return ret;
 }
@@ -242,6 +381,7 @@ void WorldTick()
             touch.depth = 0.0_r;
         }
         ball = s_world.ball;
+        bool fixed = false;
         for (int i = 0; i < s_rules.MICROTICKS_PER_TICK; ++i)
         {
             move(ball, s_microstep);
@@ -253,7 +393,12 @@ void WorldTick()
                 real_t v = ball.vel.dot(touch.normal);
                 if (v > 0)
                 {
+                    if (fixed && v > 1.0)
+                    {
+                        v = v;
+                    }
                     ball.vel -= touch.normal * (1 + ball.arena_e) * v;
+                    fixed = true;
                 }
             }
         }
@@ -356,7 +501,7 @@ void MyStrategy::act(const Robot& me, const Rules& rules, const Game& game, Acti
             target = s_home_pos + ball_dir * home_r;
         }
         else {
-            vec3 target_pos = vec3(-100.0_r * (me_pos.x / abs(me_pos.x)), 0.0_r, 0.0_r);
+            vec3 target_pos = s_goal_pos + vec3(30.0_r, 0.0_r, 0.0_r);
             vec3 ball_dir = (ball_pos - target_pos);
             ball_dir.y = 0.;
             ball_dir.normalize();
